bba98fff60578163c388979edc9abecc8490d1f4
Markus Yeo <markusyeojy@hotmail.com>
2023-02-02 12:34:40 +0800

    Update to use revchatgpt
    Using official chatgpt api now

--- src/responses.py
+++ src/responses.py
@@ -1,20 +1,12 @@
-import openai
+from revChatGPT.Official import Chatbot
 from asgiref.sync import sync_to_async
 from env import OPENAI_KEY
 
-openai.api_key = OPENAI_KEY
+
+chatbot = Chatbot(api_key=OPENAI_KEY)
 
 async def handle_response(message) -> str:
-    response = await sync_to_async(openai.Completion.create)(
-        model="text-davinci-003",
-        prompt=message,
-        temperature=0.7,
-        max_tokens=2048,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-    )
-
-    responseMessage = response.choices[0].text
+    response = await sync_to_async(chatbot.ask)(message)
+    responseMessage = response["choices"][0]["text"]
 
     return responseMessage
